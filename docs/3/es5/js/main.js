4ae68f4d8e6a7618f1d5b22482590247c78533ef
ytyaru <yttry0@gmail.com>
2025-01-10 19:53:53 +0900

    add 3

---
+++ docs/3/es5/js/main.js
@@ -0,0 +1,285 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOMContentLoaded!!');
+    const author = 'ytyaru'
+    van.add(document.querySelector('main'), 
+        van.tags.h1(van.tags.a({href:`https://github.com/${author}/Html.VanJS.ES5.DynamicImporter.20250108131626/`}, 'ES5.DynamicImporter')),
+        van.tags.p('ES5でも動作する動的インポートAPIを作る。'),
+//        van.tags.p('Create a dynamic import API that also works with ES5.'),
+    )
+    van.add(document.querySelector('footer'),  new Footer('ytyaru', '../').make())
+
+    const a = new Assertion()
+    // constructor()
+    a.t(()=>{
+        const jdl = new JsDynamicLoader()
+        return Type.isIns(jdl, JsDynamicLoader)
+            && ''===jdl.basePath
+            && Type.isEmpty(jdl.fullPaths)
+            && jdl.isIdling
+            && 0===jdl.loadedFullPaths.size
+    })
+    // constructor(options)
+    // ...
+    // add()
+    a.t(()=>{
+        const jdl = new JsDynamicLoader()
+        jdl.add('js/test-files/some-fn-0.js')
+        const paths = jdl.fullPaths
+        return Type.isIns(jdl, JsDynamicLoader)
+            && ''===jdl.basePath
+            && 1 === paths.length
+            && 'js/test-files/some-fn-0.js'=== paths[0]
+            && jdl.isQueueing
+    })
+    a.t(()=>{
+        const jdl = new JsDynamicLoader()
+        jdl.add('/js/test-files/some-fn-0.js')
+        const paths = jdl.fullPaths
+        return Type.isIns(jdl, JsDynamicLoader)
+            && ''===jdl.basePath
+            && 1 === paths.length
+            && 'js/test-files/some-fn-0.js'=== paths[0]
+            && jdl.isQueueing
+    })
+    a.t(()=>{
+        const jdl = new JsDynamicLoader()
+        jdl.add('//js/test-files/some-fn-0.js')
+        const paths = jdl.fullPaths
+        return Type.isIns(jdl, JsDynamicLoader)
+            && ''===jdl.basePath
+            && 1 === paths.length
+            && 'js/test-files/some-fn-0.js'=== paths[0]
+            && jdl.isQueueing
+    })
+    a.t(()=>{
+        const jdl = new JsDynamicLoader({basePath:'js/test-files'})
+        jdl.add('some-fn-0.js')
+        const paths = jdl.fullPaths
+        return Type.isIns(jdl, JsDynamicLoader)
+            && 'js/test-files'===jdl.basePath
+            && 1 === paths.length
+            && 'js/test-files/some-fn-0.js'=== paths[0]
+            && jdl.isQueueing
+    })
+    a.t(()=>{
+        const jdl = new JsDynamicLoader({basePath:'js/test-files/'})
+        jdl.add('some-fn-0.js')
+        const paths = jdl.fullPaths
+        return Type.isIns(jdl, JsDynamicLoader)
+            && 'js/test-files'===jdl.basePath
+            && 1 === paths.length
+            && 'js/test-files/some-fn-0.js'=== paths[0]
+            && jdl.isQueueing
+    })
+    a.t(()=>{
+        const jdl = new JsDynamicLoader({basePath:'js/test-files'})
+        jdl.add('/some-fn-0.js')
+        const paths = jdl.fullPaths
+        return Type.isIns(jdl, JsDynamicLoader)
+            && 'js/test-files'===jdl.basePath
+            && 1 === paths.length
+            && 'js/test-files/some-fn-0.js'=== paths[0]
+            && jdl.isQueueing
+    })
+    a.t(()=>{
+        const jdl = new JsDynamicLoader({basePath:'js/test-files/'})
+        jdl.add('/some-fn-0.js')
+        const paths = jdl.fullPaths
+        return Type.isIns(jdl, JsDynamicLoader)
+            && 'js/test-files'===jdl.basePath
+            && 1 === paths.length
+            && 'js/test-files/some-fn-0.js'=== paths[0]
+            && jdl.isQueueing
+    })
+    a.t(()=>{
+        const jdl = new JsDynamicLoader({basePath:'js/test-files//'})
+        jdl.add('//some-fn-0.js')
+        const paths = jdl.fullPaths
+        return Type.isIns(jdl, JsDynamicLoader)
+            && 'js/test-files'===jdl.basePath
+            && 1 === paths.length
+            && 'js/test-files/some-fn-0.js'=== paths[0]
+            && jdl.isQueueing
+    })
+    // load()
+    a.t(()=>{
+        const jdl = new JsDynamicLoader()
+        const ret = jdl.load(()=>{
+            const test = 'someFn0' in window && Type.isFn(window.someFn0) && 'Some function 0.'===someFn0()
+            a.t(test); a.fin(); console.log(test); console.assert(test);
+            jdl.clear(true)
+            delete window.someFn0
+        }, 'js/test-files/some-fn-0.js')
+//        }, 'js/test-files/some-fn-2.js')
+//        console.log(jdl.state)
+//        a.t(jdl.isSucceeded)
+        return ret
+    })
+    a.t(()=>{
+        const jdl = new JsDynamicLoader()
+        jdl.add('js/test-files/some-fn-0.js')
+        jdl.load(()=>{
+            const test = 'someFn0' in window && Type.isFn(window.someFn0) && 'Some function 0.'===someFn0()
+            a.t(test); a.fin(); console.log(test); console.assert(test);
+            jdl.clear(true)
+            delete window.someFn0
+        })
+        return true
+    })
+    a.t(()=>{
+        const jdl = new JsDynamicLoader({basePath:'js/test-files'})
+        jdl.add('some-fn-0.js')
+        jdl.load(()=>{
+            const test = 'someFn0' in window && Type.isFn(window.someFn0) && 'Some function 0.'===someFn0()
+            a.t(test); a.fin(); console.log(test); console.assert(test);
+            jdl.clear(true)
+            delete window.someFn0
+        })
+        return true
+    })
+    a.t(()=>{
+        const jdl = new JsDynamicLoader({basePath:'js/test-files///'})
+        jdl.add('///some-fn-0.js')
+        jdl.load(()=>{
+            const test = 'someFn0' in window && Type.isFn(window.someFn0) && 'Some function 0.'===someFn0()
+            a.t(test); a.fin(); console.log(test); console.assert(test);
+            jdl.clear(true)
+            delete window.someFn0
+        })
+        return true
+    })
+    // load() 複数
+    a.t(()=>{
+        const jdl = new JsDynamicLoader()
+        jdl.load(()=>{
+            for (let i=0; i<2; i++) {
+                const name = `someFn${i}`
+                const test = name in window && Type.isFn(window[name]) && `Some function ${i}.`===window[name]()
+                a.t(test); a.fin(); console.log(i, name, test); console.assert(test);
+                jdl.clear(true)
+            }
+            for (let i=0; i<2; i++) { delete window[`someFn${i}`] }
+        }, 'js/test-files/some-fn-0.js', 'js/test-files/some-fn-1.js')
+        return true
+    })
+    a.t(()=>{
+        const jdl = new JsDynamicLoader()
+        jdl.add('js/test-files/some-fn-0.js', 'js/test-files/some-fn-1.js')
+        jdl.load(()=>{
+            for (let i=0; i<2; i++) {
+                const name = `someFn${i}`
+                const test = name in window && Type.isFn(window[name]) && `Some function ${i}.`===window[name]()
+                a.t(test); a.fin(); console.log(i, name, test); console.assert(test);
+                jdl.clear(true)
+            }
+            for (let i=0; i<2; i++) { delete window[`someFn${i}`] }
+        })
+        return true
+    })
+    a.t(()=>{
+        const jdl = new JsDynamicLoader({basePath:'js/test-files'})
+        jdl.add('some-fn-0.js', 'some-fn-1.js')
+        jdl.load(()=>{
+            for (let i=0; i<2; i++) {
+                const name = `someFn${i}`
+                const test = name in window && Type.isFn(window[name]) && `Some function ${i}.`===window[name]()
+                a.t(test); a.fin(); console.log(i, name, test); console.assert(test);
+                jdl.clear(true)
+            }
+            for (let i=0; i<2; i++) { delete window[`someFn${i}`] }
+        })
+        return true
+    })
+    // onStepped
+    a.t(()=>{
+        const isOks = [false, false]
+        const jdl = new JsDynamicLoader({basePath:'js/test-files', onStepped:(e)=>{
+            if (e.target.src.endsWith('js/test-files/some-fn-0.js')) {isOks[0]=true; console.assert(isOks[1]===false); a.t(isOks[0] && !isOks[1]); a.t(jdl.isLoading); a.fin();}
+            if (e.target.src.endsWith('js/test-files/some-fn-1.js')) {isOks[1]=true; console.assert(isOks[0]===true); a.t(isOks[0] && isOks[1]); a.t(jdl.isLoading); a.fin();}
+        }})
+        jdl.add('some-fn-0.js', 'some-fn-1.js')
+        jdl.load(()=>{
+            console.log(jdl.state, jdl.isLoaded, isOks)
+            a.t(jdl.isLoaded); a.fin();
+            console.assert(isOks.every(v=>v===true))
+            jdl.clear(true)
+            for (let i=0; i<2; i++) { delete window[`someFn${i}`] }
+        })
+        return true
+    })
+    // onStepFailed
+    a.t(()=>{
+        const jdl = new JsDynamicLoader({basePath:'js/test-files', onStepFailed:(e)=>{
+            a.t(e.srcElement.src.endsWith('js/test-files/NOT-EXIST.js'))
+            a.fin()
+        }})
+        jdl.add('NOT-EXIST.js')
+        jdl.load(()=>{
+            const t1 = 1===jdl.failedFullPaths.size
+            a.t(t1); a.fin(); console.assert(t1);
+            const t2 = 0===jdl.loadedFullPaths.size
+            a.t(t2); a.fin(); console.assert(t2);
+            const t3 = [...jdl.failedFullPaths][0].endsWith('NOT-EXIST.js')
+            a.t(t3); a.fin(); console.assert(t3);
+        })
+        return true
+    })
+    // 警告：一つ以上パスを追加してください
+    a.t(()=>{
+        const jdl = new JsDynamicLoader({basePath:'js/test-files'})
+//        jdl.add('some-fn-0.js') // 一つもパスを追加しない
+        const ret = jdl.load(()=>{
+            a.t(false); a.fin(); console.assert(false);// ここは通らないはず！
+        })
+        return ret===false
+    })
+    // 成功と失敗をするファイルが混在している
+    a.t(()=>{
+        const jdl = new JsDynamicLoader({basePath:'js/test-files', paths:[`some-fn-0.js`, `NOT-EXIST.js`, `some-fn-1.js`], 
+        //const jdl = new JsDynamicLoader({basePath:'js/test-files', paths:[`NOT-EXIST.js`, `some-fn-0.js`, `some-fn-1.js`], 
+        //const jdl = new JsDynamicLoader({basePath:'js/test-files', paths:[`some-fn-0.js`, `some-fn-1.js`, `NOT-EXIST.js`], 
+            onStepFailed: (e)=>{
+                //console.log(e.target.src)
+                const test = e.target.src.endsWith(`js/test-files/NOT-EXIST.js`)
+                a.t(test); a.fin(); console.assert(test);
+            },
+            onStepped: (e)=>{
+                const paths = [0,1].map(n=>`js/test-files/some-fn-${n}.js`)
+                const test = paths.some(p=>e.target.src.endsWith(p))
+                //console.log(e.target.src)
+                a.t(test); a.fin(); console.assert(test);
+                     if (e.target.src.endsWith('js/test-files/some-fn-0.js')) {const t2 = 1===jdl.loadedFullPaths.size; a.t(t2); a.fin(); console.assert(t2);}
+                else if (e.target.src.endsWith('js/test-files/some-fn-1.js')) {const t2 = 2===jdl.loadedFullPaths.size; a.t(t2); a.fin(); console.assert(t2);}
+                else {a.t(false); a.fin(); console.assert(false);}
+            },
+            onLoaded:()=>{
+                const t1 = 2===jdl.loadedFullPaths.size
+                const t2 = 1===jdl.failedFullPaths.size
+                a.t(t1 && t2); a.fin(); console.assert(t1 && t2);
+                const t3 = [...jdl.loadedFullPaths][0].endsWith(`js/test-files/some-fn-0.js`)
+                const t4 = [...jdl.loadedFullPaths][1].endsWith(`js/test-files/some-fn-1.js`)
+                const t5 = [...jdl.failedFullPaths][0].endsWith(`js/test-files/NOT-EXIST.js`)
+                //console.log(t3, t4, t5)
+                a.t(t3 && t4 && t5); a.fin(); console.assert(t3 && t4 && t5);
+                const t6 = jdl.isLoaded && jdl.hasFailded && !jdl.isSucceeded;
+                //console.log(jdl.isLoaded, jdl.hasFailded, !jdl.isSucceeded)
+                a.t(t6); a.fin(); console.assert(t6);
+
+                for (let i=0; i<2; i++) {
+                    const name = `someFn${i}`
+                    const test = name in window && Type.isFn(window[name]) && `Some function ${i}.`===window[name]()
+                    a.t(test); a.fin(); console.log(i, name, test); console.assert(test);
+                    jdl.clear(true)
+                }
+                for (let i=0; i<2; i++) { delete window[`someFn${i}`] }
+            },
+        })
+        jdl.load();
+        return true
+    })
+    a.fin()
+});
+window.addEventListener('beforeunload', (event) => {
+    console.log('beforeunload!!');
+});
+
---
+++ docs/3/es6/js/js-dynamic-loader.js
@@ -0,0 +1,125 @@
+(function(){
+/*
+class QueueState {
+    constructor() { return Object.freeze(this) }
+    get Idling() { return 0 }     // 待機中
+    get Queueing() { return 1 }   // 待ち行列を作成中
+    get Processing() { return 2 } // 処理中（Loading）
+    get Finished() { return 3 }   // 完了済み（Loaded）
+}
+*/
+class JsDynamicLoader { // 非ESMでも動作するよう<script>タグ挿入することで動的インポートする
+    static QueueState = new QueueState();
+    constructor(options=null) {
+        this._options = options && 'object'===typeof options ? ({...this._defaultOptions, ...options}) : this._defaultOptions;
+        this._basePath = this._options.basePath
+        this._paths = this._options.paths
+        this._count = 0;
+        this._CLASS_ID = 'js-dynamic-loader'; // <script class="${CLASS_ID}">
+//        this._state = 0 < this._options.paths.size ?  JsDynamicLoader.QueueState.Queueing: JsDynamicLoader.QueueState.Idling
+    }
+    get _defaultOptions() { return {
+        basePath: '',
+        paths: new Set(),
+        loadedFullPaths: new Set(),
+        failedFullPaths: new Set(),
+        onStepFailed: (e)=>{console.error(`onStepFailed:`, e.target.src)}, 
+        onStepped: (e)=>{console.debug(`onStepped:`, e.target.src + ' is successfully loaded.');}, 
+        onLoaded: ()=>{console.debug(`Loaded: All loaded !!`);}
+    } }
+    get basePath() {return this._options.basePath}
+    set _basePath(v) {this._options.basePath = this.#delTailSlash(v)}
+    get fullPaths() {return [...this._options.paths].map(p=>this.#joinPath(p))}
+    get loadedFullPaths() {return this._options.loadedFullPaths}
+    get failedFullPaths() {return this._options.failedFullPaths}
+    set _paths(v) {
+        if (Array.isArray(v)){this._options.paths = new Set(v.map(V=>this.#delHeadSlash(V)))}
+        else if (v instanceof Set){this._options.paths = new Set([...v].map(V=>this.#delHeadSlash(V)))}
+    }
+    set _onLoaded(v){if('function'===typeof v){this._options.onLoaded=v}}
+    /*
+    get state() {return this._state}
+    get isIdling() {return this._state===JsDynamicLoader.QueueState.Idling} // add()されるのを待っている（完了後もこれ）
+    get isQueueing() {return this._state===JsDynamicLoader.QueueState.Queueing} // load()されるのを待っている
+    get isLoading() {return this._state===JsDynamicLoader.QueueState.Processing} // onLoaded()されるのを待っている
+//    get isLoaded() {return this._state===JsDynamicLoader.QueueState.Finished}
+//    get isSucceeded() {return this.isLoaded && 0===this._options.failedFullPaths.size && 0 < this._options.loadedFullPaths.size}
+//    get hasFailded() {return 0 < this._options.failedFullPaths.size}
+    */
+    #isProcessed(path) {
+        const fullPath = this.#joinPath(path)
+        if (this._options.loadedFullPaths.has(fullPath)) {console.error('************');return true}
+        if (this._options.failedFullPaths.has(fullPath)) {console.error('VVVVVVVVVVVV');return true}
+        return false
+    }
+    add(...paths) {
+        // ロードに成功・失敗したパスは追加しない
+        for (let p of [...paths].map(p=>this.#delHeadSlash(p)).filter(p=>!this.#isProcessed(p))) { this._options.paths.add(p) }
+//        for (let p of paths) { this._options.paths.add(this.#delHeadSlash(p)) }
+//        if (0 < this._options.paths.size) {this._state = JsDynamicLoader.QueueState.Queueing}
+    }
+    clear(isRemoveScript=false) { // isRemoveScript:読込済の<script>要素を削除する
+        if (isRemoveScript) {
+            for (let el of document.querySelectorAll(`script.${this._CLASS_ID}`)) {
+                el.remove();
+            }
+            this._options.loadedFullPaths.clear();
+            this._options.failedFullPaths.clear();
+        }
+        this._count=0;
+        this._options.paths.clear()
+//        this._state = JsDynamicLoader.QueueState.Idling
+    }
+    load(onLoaded, ...paths) { // 引数なしでもいい。constructor, addで設定できるから。
+        this._onLoaded = onLoaded
+        this.add(...paths)
+        console.log(this._options.paths)
+        //if (!this.isQueueing) {console.warn(`一つ以上パスを追加してください。パスを追加する方法は三通りあります。
+        if (0===this._options.paths.size) {console.warn(`一つ以上パスを追加してください。パスを追加する方法は三通りあります。
+A. コンストラクタの第一引数に{paths:['some/path']}を渡す
+B. load()を呼ぶ前にadd('some/path')を呼ぶ
+C. load()の第二引数以降に渡す：load(()=>{alert('Loaded')}, 'some/path')`);return false;}
+//        this._state = JsDynamicLoader.QueueState.Processing
+        for (let path of this._options.paths) {
+            this.#insertEl(this.#createEl(path))
+        }
+        return true
+    }
+    #onProcess(e, isError=false) { // <script onload="..."> 一つのファイルを読み込む毎に行う
+        this._count++;
+        this._options[`${isError ? 'fail' : 'load'}edFullPaths`].add(e.target.src)
+        this._options[`on${isError ? 'StepFailed' : 'Stepped'}`](e)
+        if (this._count < this._options.paths.size) {return;} // 完了判定
+//        this._state = JsDynamicLoader.QueueState.Finished
+        //this._state = JsDynamicLoader.QueueState.Idling
+        //this._options.onLoaded()
+//        this._options.onLoaded([...this._options.loadedFullPaths], [...this._options.failedFullPaths])
+//        this.clear()
+        const loadedFullPaths = [...this._options.loadedFullPaths]
+        const failedFullPaths = [...this._options.failedFullPaths]
+        this.clear()
+        this._options.onLoaded(loadedFullPaths, failedFullPaths)
+        // results: [{isSucceeded:true, fullPath:'...'}, ...]
+    }
+    #createEl(path) {
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = this.#joinPath(path)
+        script.classList.add(this._CLASS_ID)
+        script.onload = (e)=>{ this.#onProcess(e); }
+        script.onerror = (e)=>{ this.#onProcess(e, true); }
+        return script
+    }
+    #insertEl(el) {
+        const s = document.querySelector('script')
+        if (s) {s.parentNode.insertBefore(el, s); return;}
+        const h = document.querySelector('head')
+        if (h) {h.append(el); return;}
+        document.querySelector('body').append(el)
+    }
+    #joinPath(path) {return this._options.basePath ? this._options.basePath + '/' + path : path; }
+    #delHeadSlash(str) {return str.replace(/^(\/{1,})/,'')}
+    #delTailSlash(str) {return str.replace(/(\/{1,})$/,'')}
+}
+window.JsDynamicLoader = JsDynamicLoader 
+})();
